2a9f49150958372cef7e242ad2da2a098cc868b8
Hugo Gutierrez <analistafuncional@SERVOSA.PE>
2024-07-21 17:30:04 -0500

    metodo sav y mover el insert , proceso de transaccion con commit y rollback

--- Logica/LEstudiantes.cs
+++ Logica/LEstudiantes.cs
@@ -16,6 +16,7 @@ namespace Logica
         private List<TextBox> listtextbox;
         private List<Label> listtextlabel;
         private PictureBox image;
+        private string _accion = "insert";
         //private Librarys librarys;
 
         //metodo CONSTRUCTOR que recibe un paramaetro un objeto de tipo textbos de tipo lista de tipo estudiante
@@ -47,7 +48,7 @@ namespace Logica
                 {
                     if (listtextbox[2].Text.Equals(""))
                     {
-                        listtextlabel[2].Text = "Indique su a";
+                        listtextlabel[2].Text = "Indique su apellido";
                         listtextlabel[2].ForeColor = Color.Red;
                         listtextbox[2].Focus();
                     }
@@ -55,7 +56,7 @@ namespace Logica
                     {
                         if (listtextbox[3].Text.Equals(""))
                         {
-                            listtextlabel[3].Text = "Indique su m";
+                            listtextlabel[3].Text = "Indique su mail";
                             listtextlabel[3].ForeColor = Color.Red;
                             listtextbox[3].Focus();
                         }
@@ -66,17 +67,7 @@ namespace Logica
                                 listtextlabel[3].Text = "mail valido";
                                 listtextlabel[3].ForeColor = Color.Black;
 
-                                var imagenarray = uploadImage.ImageToByte(image.Image);//Image con I en mayusc es una clase de sistema para decirle que es un valor te tipo IMAGEN como tal osea que lo TRANSFORME EN UNA IMAGEN COMO ES NO EN BINARIO NO EN EXA SI NO EN IMAGEN IMAGEN QUE LA TRATE COMO TAL entonces  se e pone .IMAGE asi como .TEXT o .true OSEA ES UNA EXTENSION DE image,
-                                                                                                //a la image que es la local y toma esa propiedad entonces la pasamos a
-                                                                                                //un imagetobyte el proceso requiere de una imagen para que sea pasado a un array que previamente pedia un metodo
-                                                                                                //en esta caso la clase se crea con un byte y con un [] que rerpresenta el array
-                                                                                                //entonces se le pasa un formato Imagetobyte de tipo IMAGEN = Image cual recibe el objeto del mismo nombre y se le asigna el formato byte en array
-                                _Estudiante.Value(e => e.dni, listtextbox[0].Text)
-                                                                    .Value(e => e.nombre, listtextbox[1].Text)
-                                                                    .Value(e => e.apellido, listtextbox[2].Text)
-                                                                    .Value(e => e.email, listtextbox[3].Text)
-                                                                    .Value(e => e.image, imagenarray)
-                                                                    .Insert();
+
                             }
                             else
                             {
@@ -90,6 +81,37 @@ namespace Logica
                 }
             }
 
+        }
+
+        private void save()
+        {
+            BeginTransactionAsync();
+            try
+            {
+                var imagenarray = uploadImage.ImageToByte(image.Image);//Image con I en mayusc es una clase de sistema para decirle que es un valor te tipo IMAGEN como tal osea que lo TRANSFORME EN UNA IMAGEN COMO ES NO EN BINARIO NO EN EXA SI NO EN IMAGEN IMAGEN QUE LA TRATE COMO TAL entonces  se e pone .IMAGE asi como .TEXT o .true OSEA ES UNA EXTENSION DE image,
+                                                                       //a la image que es la local y toma esa propiedad entonces la pasamos a
+                                                                       //un imagetobyte el proceso requiere de una imagen para que sea pasado a un array que previamente pedia un metodo
+                                                                       //en esta caso la clase se crea con un byte y con un [] que rerpresenta el array
+                                                                       //entonces se le pasa un formato Imagetobyte de tipo IMAGEN = Image cual recibe el objeto del mismo nombre y se le asigna el formato byte en array
+                switch(_accion)
+                {
+                    case "insert":
+                        _Estudiante.Value(e => e.dni, listtextbox[0].Text)
+                                    .Value(e => e.nombre, listtextbox[1].Text)
+                                    .Value(e => e.apellido, listtextbox[2].Text)
+                                    .Value(e => e.email, listtextbox[3].Text)
+                                    .Value(e => e.image, imagenarray)
+                                    .Insert();
+                        break;
+
+                }
+                CommitTransaction();
+                //Restablecer();
+            }
+            catch (Exception)
+            {
+                RollbackTransaction();
+            }
         }
     }
 }
